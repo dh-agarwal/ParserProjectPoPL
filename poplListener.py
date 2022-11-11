104dc09066095f2a94b6da81f6326eef62e134b3
Ashwin <ashwinprayaga@gmail.com>
2022-11-10 21:56:33 -0600

    Using ANTLR Files now & added example grammar

---
+++ poplListener.py
@@ -0,0 +1,21 @@
+# Generated from popl.g4 by ANTLR 4.9.2
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .poplParser import poplParser
+else:
+    from poplParser import poplParser
+
+# This class defines a complete listener for a parse tree produced by poplParser.
+class poplListener(ParseTreeListener):
+
+    # Enter a parse tree produced by poplParser#hi.
+    def enterHi(self, ctx:poplParser.HiContext):
+        pass
+
+    # Exit a parse tree produced by poplParser#hi.
+    def exitHi(self, ctx:poplParser.HiContext):
+        pass
+
+
+
+del poplParser
