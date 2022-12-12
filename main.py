19c1c0c91f2622ea5f6f0b53b7fae9d683fa1175
Ashwin <ashwinprayaga@gmail.com>
2022-12-11 21:22:10 -0600

    Added antlr 4.11 jar for GUI

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # Ping Pong Club Python file
 
 # --- IMPORTS ---
+from shutil import which
 import sys
 from antlr4 import *
 from antlr4.tree.Trees import Trees #Tree visualization in text
 from poplLexer import poplLexer
 from poplParser import poplParser
+import subprocess
 
 # --- MAIN ---
 def main(argv):
@@ -20,6 +22,15 @@ def main(argv):
     tree = parser.start()
 
     print(tree.toStringTree(recog=parser))
+    print('')
+
+    p = subprocess.Popen([which("java"), '-cp', 'antlr-4.11.1-complete.jar', 'org.antlr.v4.gui.Interpreter', 'popl.g4', 'start', '-gui'])
+    out, err = p.communicate()
+    out = out.decode("UTF-8")
+    err = err.decode("UTF-8")
+
+    if err: print(err, end='')
+    if out: print(out, end='')
 
 
 if __name__ == "__main__":
